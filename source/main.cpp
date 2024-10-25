142b76fe7d6b14b2a39afc0c49a86fec04d329a9
D0NIFF <abdulmalikgeraev59@gmail.com>
2024-10-25 14:50:01 +0300

    Add texture load

--- source/main.cpp
+++ source/main.cpp
@@ -1,12 +1,14 @@
 #pragma once
 
 #include <iostream>
-#include <GL/glew.h>
 
-#include "GLFW/glfw3.h"
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+
 #include "window/Window.h"
 #include "window/Event.h"
 #include "graphic/Shader.h"
+#include "graphic/Texture.h"
 
 
 int WIDTH = 1280;
@@ -25,10 +27,10 @@ float vertices[] = {
 
 int main()
 {
-    Window::initialize(1280, 720, "Minecraft");
+    Window::initialize(WIDTH, HEIGHT, "Minecraft");
     Event::initialize();
 
-    Graphic::Shader* shader = Graphic::loadShader(RESOURCES_PATH "shaders/main.glslv", RESOURCES_PATH "shaders/main.glslf");
+    Graphic::Shader* shader = Graphic::loadShader(RESOURCES_PATH "shaders/main.vert", RESOURCES_PATH "shaders/main.frag");
     if(shader == nullptr)
     {
         std::cerr << "Failed to load shaders" << std::endl;
@@ -36,19 +38,37 @@ int main()
         return -1;
     }
 
+    Graphic::Texture* texture = Graphic::Texture::loadTexture(RESOURCES_PATH "textures/test.png");
+    if(texture == nullptr)
+    {
+        std::cerr << "Failed to load textures" << std::endl;
+        delete shader;
+        Window::terminate();
+        return -1;
+    }
+
+
+    // Create VAO
     GLuint VAO, VBO;
     glGenVertexArrays(1, &VAO);
     glGenBuffers(1, &VBO);
 
     glBindVertexArray(VAO);
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_DYNAMIC_DRAW);
-
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 4 * sizeof(float), (GLvoid*)(0 * sizeof(GLfloat)));
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+    // position
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(GLfloat), (GLvoid*)(0 * sizeof(GLfloat)));
     glEnableVertexAttribArray(0);
+    glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(GLfloat), (GLvoid*)(3 * sizeof(GLfloat)));
+    glEnableVertexAttribArray(1);
 
     glBindVertexArray(0);
 
+    glClearColor(0.6f,0.62f,0.65f,1);
+
+    glEnable(GL_CULL_FACE);
+    glEnable(GL_BLEND);
+    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
 
     while (!Window::isShouldClose()) {
         Event::pullEvents();
@@ -57,18 +77,26 @@ int main()
             Window::setShouldClose(true);
 
         if(Event::jclicked(GLFW_MOUSE_BUTTON_1))
-            glClearColor(0.6f, 0.0f, 0.0f, 1.0f);
+        {
+            glClearColor(1.0f, 0.2467f, 0.3164f, 1.0f);
+        }
+
         glClear(GL_COLOR_BUFFER_BIT);
 
         shader->use();
+        texture->bind();
         glBindVertexArray(VAO);
-        glDrawArrays(GL_TRIANGLES, 0, 3);
+        glDrawArrays(GL_TRIANGLES, 0, 6);
         glBindVertexArray(0);
 
         Window::swapBuffers();
     }
 
-    //glfwDestroyWindow(window);
+    delete shader;
+    delete texture;
+
+    glDeleteBuffers(1, &VBO);
+    glDeleteVertexArrays(1, &VAO);
     Window::terminate();
     return 0;
 }

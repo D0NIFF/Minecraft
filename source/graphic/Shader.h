2017cc1177e5a8510a5fbbecf40252346db84c13
D0NIFF <abdulmalikgeraev59@gmail.com>
2024-10-26 09:41:48 +0300

    Added a new method uniformMatrix()

--- source/graphic/Shader.h
+++ source/graphic/Shader.h
@@ -1,16 +1,18 @@
 #ifndef SHADER_H
 #define SHADER_H
+#include <glm/fwd.hpp>
 
 namespace Graphic {
 
 class Shader {
 public:
-    unsigned int ID;
+    unsigned int id;
 
     explicit Shader(unsigned int id);
     ~Shader();
 
     void use() const;
+    void uniformMatrix(const char* name, glm::mat4 matrix);
 };
 
 extern Shader* loadShader(const char* vertexPath, const char* fragmentPath);

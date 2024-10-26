2f659d33c403a1ac220b16e1b31dcbe9ce42dd4a
D0NIFF <abdulmalikgeraev59@gmail.com>
2024-10-26 06:52:14 +0300

    Removed not used includes, replace cout to Log

--- source/graphic/Texture.cpp
+++ source/graphic/Texture.cpp
@@ -1,10 +1,10 @@
 #include "Texture.h"
 
-#include <iostream>
-#include <ostream>
+#include <format>
 #include <GL/glew.h>
 
 #include "../system/Loader.h"
+#include "../system/Log.h"
 
 namespace Graphic {
     Texture::Texture(const unsigned int id, const int width, const int height): id(id), width(width), height(height)
@@ -21,12 +21,12 @@ namespace Graphic {
         glBindTexture(GL_TEXTURE_2D, id);
     }
 
-    Texture* Texture::loadTexture(const char* path)
+    Texture* loadTexture(const char* path)
     {
         int width, height;
         const GLuint texture = System::Loader::loadPng(path, &width, &height);
         if (texture == 0){
-            std::cerr << "Could not load texture " << path << std::endl;
+            System::Log::error(std::format("Could not load texture {}", path));
             return nullptr;
         }
         return new Texture(texture, width, height);
